aa6ee75ec9f547b345282675444d04688490ac95
ramkumawat-ITG <ramkumawat@itgeeks.com>
2025-08-12 16:46:44 +0530

    attempt 13

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -4,9 +4,8 @@ import {
   AppDistribution,
   shopifyApp,
 } from "@shopify/shopify-app-remix/server";
-import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import prisma from "./db.server";
-
+import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
+import "./db.server"
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
@@ -14,11 +13,14 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(prisma),
+  sessionStorage: new MongoDBSessionStorage(
+    process.env.MONGO_URI,
+  ),
   distribution: AppDistribution.AppStore,
   future: {
     unstable_newEmbeddedAuthStrategy: true,
-    removeRest: true,
+    // Ensure REST resources are available
+    removeRest: false,
   },
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
